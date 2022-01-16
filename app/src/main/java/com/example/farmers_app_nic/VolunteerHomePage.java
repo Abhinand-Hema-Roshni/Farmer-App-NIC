24674d2ffb69b142b7bb061e0f56d70abee8a339
Roshni-Bala <Roshni-Bala@users.noreply.github.com>
2022-01-16 12:12:35 +0530

    existing farmer list UI addition

--- app/src/main/java/com/example/farmers_app_nic/VolunteerHomePage.java
+++ app/src/main/java/com/example/farmers_app_nic/VolunteerHomePage.java
@@ -1,6 +1,9 @@
 package com.example.farmers_app_nic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,16 @@ public class VolunteerHomePage extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.volunteer_homepage);
+
+        Button existing_farmer_btn;
+        existing_farmer_btn = findViewById(R.id.existing_farmer_button);
+
+        existing_farmer_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), farmer_list.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/farmers_app_nic/farmer_list.java
@@ -0,0 +1,14 @@
+package com.example.farmers_app_nic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class farmer_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_list);
+    }
+}
