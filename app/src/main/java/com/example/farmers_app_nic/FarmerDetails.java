bccd04388665cdf434ed4711801e837e7eda1b8f
AbhinandG <81344251+AbhinandG@users.noreply.github.com>
2022-01-17 00:20:40 +0530

    farmer details and trader details

---
+++ app/src/main/java/com/example/farmers_app_nic/FarmerDetails.java
@@ -0,0 +1,14 @@
+package com.example.farmers_app_nic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmerDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.farmer_details);
+    }
+}
