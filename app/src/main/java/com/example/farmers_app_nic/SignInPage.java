078aa5e6024d537652cb708f54960546ad466ac4
hema <hemalathamadeswaran@yahoo.com>
2022-02-20 22:04:57 +0800

    login otp using firebase added

--- app/src/main/java/com/example/farmers_app_nic/SignInPage.java
+++ app/src/main/java/com/example/farmers_app_nic/SignInPage.java
@@ -1,29 +1,142 @@
 package com.example.farmers_app_nic;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignInPage extends AppCompatActivity {
+
+
+    Button signin_btn;
+    Button send_otp;
+    EditText mobile_number, otp_entered;
+    String phonenumber;
+    String otpid;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_page);
 
-        Button signin_btn;
+        //declaring the relation w xml page
+        send_otp = findViewById(R.id.send_otp);
+        mobile_number  = findViewById(R.id.mobile_editText);
+        otp_entered = findViewById(R.id.otp_editText);
         signin_btn = findViewById(R.id.signin_submit_button);
+        mAuth=FirebaseAuth.getInstance();
+
+        //get texts
+
+
+  //sign in button
+        send_otp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phonenumber = mobile_number.getText().toString();
+
+                //TODO: query the postgres db to check of the number exists then only intiate otp
+                initiateotp();
+
+
+
+//                Intent intent = new Intent(view.getContext(), VolunteerHomePage.class);
+//                startActivity(intent);
+            }
+        });
+//to send the otp
         signin_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), VolunteerHomePage.class);
-                startActivity(intent);
+
+                if(otp_entered.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(),"Blank Field can not be processed",Toast.LENGTH_LONG).show();
+                else if(otp_entered.getText().toString().length()!=6)
+                    Toast.makeText(getApplicationContext(),"INvalid OTP",Toast.LENGTH_LONG).show();
+                else
+                {
+                    PhoneAuthCredential credential=PhoneAuthProvider.getCredential(otpid,otp_entered.getText().toString());
+                    signInWithPhoneAuthCredential(credential);
+                    System.out.println("success");
+
+
+             Intent intent = new Intent(view.getContext(), VolunteerHomePage.class);
+            startActivity(intent);
+
+
+                }
+
             }
         });
 
+    }
+
+    private void initiateotp() {
+
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phonenumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks()
+                {
+                    @Override
+                    public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken)
+                    {
+                        otpid=s;
+                    }
+
+
+
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential)
+                    {
+                        signInWithPhoneAuthCredential(phoneAuthCredential);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+                        Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(),"CORRECT",Toast.LENGTH_LONG).show();
+                            //startActivity(new Intent(manageotp.this,dashboard.class));
+                            finish();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Signin Code Error",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
